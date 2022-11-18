fd2aa176c79833e5cab367e0d8dc9046b2c6b429
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 15:35:58 +0100

    Add 4th bug hunting problem

---
+++ BugHunting4/src/hunting/Main.java
@@ -0,0 +1,15 @@
+package hunting;
+
+public class Main {
+	public String getStrippedSubstring() {
+		int sstart = start, ssend = end;
+		while (sstart < ssend) {
+			char c = input.charAt(sstart);
+			if(c != ' ' || c != '\n' || c != '\r' || c != '\t') {
+				break;
+			}
+			++sstart;
+		}
+		....
+	}
+}
