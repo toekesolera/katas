0cd8d33116bd565a7788fff0782f15a36b4c3466
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 15:52:31 +0100

    Fix 6th bug hunting problem
    We need to be absolutely certain that the file does not exist, so both conditions need to be fulfilled

--- BugHunting6/src/hunting/Main.java
+++ BugHunting6/src/hunting/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public void putToCache(PutRecordsRequest putRecordsRequest)
 	{
 		....
-		if (dataTmpFile == null || !dataTmpFile.exists()) {
+		if (dataTmpFile == null && !dataTmpFile.exists()) {
 			try {
 				dataTmpFile.createNewFile();
 			}
