70c4840bc7d1c22e9af26fee7f5dd89181496ed5
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 16:39:21 +0100

    Add Extra bug hunting problem

---
+++ BugHuntingExtra/src/hunting/Main.java
@@ -0,0 +1,25 @@
+package hunting;
+
+import java.util.Objects;
+
+public class Main {
+	@Override
+	public boolean equals(Object o) {
+		....
+		CheckpointStatistics that = (CheckpointStatistics) o;
+		return id == that.id &&
+				savepoint == that.savepoint &&
+				triggerTimestamp == that.triggerTimestamp &&
+				lastestAckTimestamp == that.latestAckTimestamp &&
+				stateSize == that.stateSize &&
+				duration == that.duration &&
+				alignmentBuffered == that.alignmentBuffered &&
+				processedData == processedData &&
+				persistedData == that.persistedData &&
+				numSubtasks == that.numSubtasks &&
+				numAckSubtasks == that.numAckSubtasks &&
+				status == that.status &&
+				Objects.equals(checkpointType, that.checkpointType) &&
+				Objects.equals(checkpointStatisticsPerTask, that.checkpointStatisticsPerTask);
+	}
+}
