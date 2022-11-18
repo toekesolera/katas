11dbd52810d45aeb8d2bd335a138630e16dd1644
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 15:50:42 +0100

    Add 6th bug hunting problem

---
+++ BugHunting6/src/hunting/Main.java
@@ -0,0 +1,19 @@
+package hunting;
+
+public class Main {
+	@Override
+	public void putToCache(PutRecordsRequest putRecordsRequest)
+	{
+		....
+		if (dataTmpFile == null || !dataTmpFile.exists()) {
+			try {
+				dataTmpFile.createNewFile();
+			}
+			catch (IOException e) {
+				LOGGER.error("Failed to create cache tmp file, return.", e);
+				return;
+			}
+		}
+		....
+	}
+}
