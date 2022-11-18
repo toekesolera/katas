d13269d65b146363c927f112313d8f3b4d528fda
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 15:28:36 +0100

    Fix 3rd problem bug
    Size cannot be smaller than 0

--- BugHunting3/src/hunting/Main.java
+++ BugHunting3/src/hunting/Main.java
@@ -4,7 +4,7 @@ protected PreDeConModel computeLocalModel(DoubleDBIDList neighbors, ....) {
 	final int referenceSetSize = neighbors.size();
 	....
 	// Shouldn't happen:
-	if(referenceSetSize < 0) {
+	if(referenceSetSize < 1) {
 		LOG.warning("Empty reference set - should at least include the query point!");
 		return new PreDeConModel(Integer.MAX_VALUE, DBIDUtil.EMPTYDBIDS);
 	}
