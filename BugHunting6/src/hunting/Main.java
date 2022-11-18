f894d944cc9952eecb4dace6abc8debe755c9dcb
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 16:10:30 +0100

    Fix 6th bug hunting problem
    Leaving the possibility for x to be the same size as the length of index_64 would cause out of bounds exceptions

--- BugHunting6/src/hunting/Main.java
+++ BugHunting6/src/hunting/Main.java
@@ -2,7 +2,7 @@ package hunting;
 
 public class Main {
 	private static byte char64(char x) {
-		if ((int)x < 0 || (int)x > index_64.length)
+		if ((int)x < 0 || (int)x >= index_64.length)
 			return -1;
 		return index_64[(int)x];
 	}
