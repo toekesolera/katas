a2c3c87bdab474ff8c42a2282da9ef21ce060425
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 15:28:02 +0100

    Add 3rd bug hunting problem

---
+++ BugHunting3/src/hunting/Main.java
@@ -0,0 +1,11 @@
+package hunting;
+
+protected PreDeConModel computeLocalModel(DoubleDBIDList neighbors, ....) {
+	final int referenceSetSize = neighbors.size();
+	....
+	// Shouldn't happen:
+	if(referenceSetSize < 0) {
+		LOG.warning("Empty reference set - should at least include the query point!");
+		return new PreDeConModel(Integer.MAX_VALUE, DBIDUtil.EMPTYDBIDS);
+	}
+}
