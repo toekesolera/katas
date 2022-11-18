3ef55eb3912531fbe21590d1f449a0623ff8c496
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 15:15:19 +0100

    Fix bug
    It would otherwise always compare bounds[j+1] with itself, making the comparison pointless

--- BugHunting1/src/hunting/Main.java
+++ BugHunting1/src/hunting/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		for(int i = 0, j = 0; i < e; i++, j += 2) {
 			if (dims[i] != other.dims[i] 
 					|| bounds[j] != other.bounds[j]
-					|| bounds[j + 1] != bounds[j + 1]) {
+					|| bounds[j + 1] != other.bounds[j + 1]) {
 				return false;
 			}
 		}
