6ade1a8dff327615db387f37346bc76954b4cef4
AlexandruCotofan <alexandru0cotofan@gmail.com>
2022-11-18 16:34:05 +0100

    Fix 5th bug hunting problem
    There is no unsigned byte in Java. The solution is to cast it to int and mask it with 0xff to "convert" it into "unsigned byte".

--- BugHunting5/src/hunting/Main.java
+++ BugHunting5/src/hunting/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static final ByteSequence prefixEndOf(ByteSequence prefix) {
 		byte[] endKey = prefix.getBytes().clone();
 		for (int i = endKey.length - 1; i >= 0; i--) {
-			if (endKey[i] < 0xff) {
+			if (((int)endKey[i] & 0xff) < 0xff) {
 				endKey[i] = (byte) (endKey[i] + 1);
 				return ByteSequence.from(Arrays.copyOf(endKey, i + 1));
 			}
